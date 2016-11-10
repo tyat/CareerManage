23d51f17e69dea45c0c60d6f67ea3118454a2180
lytai2013@163.com <702431328@qq.com>
2016-11-10 20:21:20 +0800

    Ver 11.10

---
+++ src/main/java/com/service/JobService.java
@@ -0,0 +1,7 @@
+package com.service;
+
+/**
+ * Created by w on 2016/11/5.
+ */
+public class JobService {
+}
---
+++ src/main/java/com/service/UnempService.java
@@ -0,0 +1,7 @@
+package com.service;
+
+/**
+ * Created by w on 2016/11/10.
+ */
+public class UnempService {
+}
